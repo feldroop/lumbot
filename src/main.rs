4de7686facb201fe402faad9f5c0fdbaf117c56f
Felix Droop <fldroop@gmail.com>
2021-03-13 16:36:51 +0100

    Added pixel value JSON file

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
 use structopt::StructOpt;
+use std::fs::File;
+use std::io::prelude::*;
+use serde::Deserialize;
 
 use tokio::time::{sleep, Duration};
-use tokio::{io::AsyncReadExt, fs::File};
-
 use fantoccini::{ClientBuilder, Locator};
 
 use winapi::um::{wingdi::GetPixel, winuser::GetDC};
 use std::ptr::null_mut;
-
-// Coordinates of the right branches (hardcoded for screen and wedriver)
-const BRANCH_X: i32 = 369;
-const BRANCH_YS: [i32; 6] = [685, 585, 485, 385, 285, 185];
-
-// color value of a pixel inside a branch
-const BROWN: u32 = 3699873;
 
 #[derive(Debug, StructOpt)]
 struct Cli {
@@ -23,29 +17,44 @@ struct Cli {
     /// the delay after a click
     #[structopt(long = "milliseconds-delay", short = "m", default_value = "250")]
     delay: u64,
+    /// The path to a file with the pixel values for your lumberjack game screen/webdriver setup
+    #[structopt(long = "pixel-file", short = "p")]
+    pixel_file: String,
     /// The path to a file with the url to the website of the desired lumberjack game
     #[structopt(long = "url-file", short = "u")]
     url_file: String,
+}
+
+#[derive(Deserialize)]
+struct PixelValues {
+    /// x coordinate of a pixel inside the right branches of the the tree (regarding the whole screen)
+    right_branch_x: i32,
+    /// y coordinates of pixels inside each of the six branches of the tree (regarding the whole screen)
+    branch_ys: [i32; 6],
+    /// the RGBA value of a pixel inside a branch as a 32 bit integer
+    brown: u32
 }
 
 #[tokio::main]
 async fn main() -> Result<(), fantoccini::error::CmdError> {
     let args = Cli::from_args();
 
-    // establish connection to webdriver
+    let pixel_file = std::fs::File::open(&args.pixel_file)
+        .expect("The file with the pixel values could not be opened.");
+    let px: PixelValues = serde_json::from_reader(pixel_file)
+        .expect("Failed to parse to pixel value file");
+
+    let mut url_file = File::open(&args.url_file)
+        .expect("The file with the url could not be opened.");
+    let mut url = String::new();
+
+    url_file.read_to_string(&mut url)
+        .expect("The file with the url could not be read.");
+
     let mut c = ClientBuilder::rustls()
         .connect(&args.driver)
         .await
         .expect("failed to connect to WebDriver");
-    
-    let mut f = File::open(&args.url_file)
-        .await
-        .expect("The file with the url could not be opened.");
-    let mut url = String::new();
-
-    f.read_to_string(&mut url)
-        .await
-        .expect("The file with the url could not be read.");
 
     // setup game
     c.goto(&url.trim()).await?;
@@ -53,28 +62,27 @@ async fn main() -> Result<(), fantoccini::error::CmdError> {
     c.set_window_position(0, 0).await?;
     c.find(Locator::Css(".button")).await?.click().await?;
     
-    // find important elements
     let right = c.find(Locator::Css("#button_right.button")).await?;
     let left = c.find(Locator::Css("#button_left.button")).await?;
     
     // sleep to give the game some time for start-up
     sleep(Duration::from_millis(1000)).await;
 
-    // get handle to the current screen
-    let hdc = unsafe {
+    // get handle to the current screen from windows
+    let hdc = unsafe { 
         GetDC(null_mut())
     };
 
     loop {
         // get colors at pixel positions of right branches
-        let colors: Vec<u32> = BRANCH_YS
+        let colors: Vec<u32> = px.branch_ys
             .iter()
-            .map(|y| unsafe { GetPixel(hdc, BRANCH_X, *y)})
+            .map(|y| unsafe { GetPixel(hdc, px.right_branch_x, *y)})
             .collect();
 
         // check if color is brown -> there is a branch on the right
         for color in colors {
-            if color == BROWN {
+            if color == px.brown {
                 left.clone().click().await?;
                 left.clone().click().await?;
             } else {
